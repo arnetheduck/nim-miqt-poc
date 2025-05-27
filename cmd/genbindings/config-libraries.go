9ffd5ed51bfaec2860fdc8006b064f45e3a5c7fa
Jacek Sieka <arnetheduck@gmail.com>
2025-05-27 14:55:20 +0200

    Generate bindings for Qml/Quick/WebEngineQuick
    Introductionary mapping of these modules across all supported Qt
    versions - to actually use the bindings productively, more work is
    needed, specially in the QMetaObject department.

--- cmd/genbindings/config-libraries.go
+++ cmd/genbindings/config-libraries.go
@@ -29,6 +29,8 @@ func ProcessLibraries(clangBin, outDir, extraLibsDir string) {
 
 			"Pdf", "PdfWidgets",
 
+			"Qml", "Quick",
+
 			// TODO Not sure where these are from :)
 			"WebKit", "WebKitWidgets"}
 
@@ -56,11 +58,11 @@ func ProcessLibraries(clangBin, outDir, extraLibsDir string) {
 			"Sql",
 
 			"PrintSupport", "Svg", "SvgWidgets", "SpatialAudio", "WebChannel",
-			"WebEngineCore", "WebEngineWidgets",
+			"WebEngineCore", "WebEngineQuick", "WebEngineWidgets",
 
 			"Pdf", "PdfWidgets",
 
-			"Qml",
+			"Qml", "Quick",
 		}
 
 		for _, lib := range libs6 {
